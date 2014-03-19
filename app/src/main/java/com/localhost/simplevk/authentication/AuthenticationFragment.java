96fcedd1c79f5379978c4d3e5c8d1c418b804bfa
localhostEU <xlocalhost@gmail.com>
2014-03-19 21:56:55 +0400

    Added and configured AndroidAnnotations library

---
+++ app/src/main/java/com/localhost/simplevk/authentication/AuthenticationFragment.java
@@ -0,0 +1,9 @@
+package com.localhost.simplevk.authentication;
+
+import android.support.v4.app.Fragment;
+
+import org.androidannotations.annotations.EFragment;
+
+@EFragment
+public class AuthenticationFragment extends Fragment{
+}
